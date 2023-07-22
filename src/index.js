32d0e2ba00f73ccb7142122bf1c0c597c905745f
Shivam Garg <gargshivam963963@gmail.com>
2023-07-22 15:17:06 +0530

    code cleanup

--- src/index.js
+++ src/index.js
@@ -4,15 +4,8 @@ import { createRoot } from "react-dom/client";
 
 import './index.css';
 
-// import { Provider } from "react-redux";
-
 const root = createRoot(document.getElementById("root"));
-// const store = createStore(Reducer);
 
 root.render(
-  // <React.StrictMode>
-  // <Provider store={store}>
   <App />
-  // </Provider>
-  // {/* </React.StrictMode> */}
 );
